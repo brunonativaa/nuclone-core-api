abbba49fb5f104110af0414400715bfeb4cd3ce0
Bruno <brunoferreirarodrigues26@outlook.com>
2026-07-23 14:24:44 -0300

    fix(schemas): ajusta lógica de validação de CPF e adiciona regras para senha

--- src/api/schemas/modules/cliente/cliente_schema.py
+++ src/api/schemas/modules/cliente/cliente_schema.py
@@ -1,14 +1,17 @@
-from pydantic import BaseModel, validator
+import re
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 
 class ClienteCreateInput(BaseModel):
-    nome: str
-    cpf:  str
-    email: str
-    senha: str
+    nome: str = Field(..., min_length=3, max_length=60, description="Nome completo")
+    cpf:  str = Field(..., description="CPF sem pontos ou traços(apenas 11 digitos)")
+    email: EmailStr = Field(..., description="E-mail válido do cliente")
+    senha: str = Field(..., min_length=8, description="Senha de usuario (mínimo 8 caracteres)")
 
-    @validator("cpf")
-    def validar_cpf(cls, value):
-        if len(value) != 11 or not value.isdigit():
+    @field_validator("cpf")
+    @classmethod
+    def validar_cpf(cls, value: str) -> str:
+        cpf_limpo = re.sub(r'\D', '', value)
+        if len(cpf_limpo) != 11:
             raise ValueError("CPF deve conter exatamente 11 dígitos")
-        return value
+        return cpf_limpo
