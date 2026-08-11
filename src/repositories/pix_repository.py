796786916eaa182fa86313af1e54d55f9780b146
Bruno <brunoferreirarodrigues26@outlook.com>
2026-08-11 15:24:45 -0300

    feat: Successful execution of the test in pytest and updating service files and repositories

--- src/repositories/pix_repository.py
+++ src/repositories/pix_repository.py
@@ -12,21 +12,31 @@ class PixRepository:
     def update_saldo(self, id_conta, valor):
         saldo = self.db.query(SaldoContaModel).filter_by(
             id_conta=id_conta).first()
+
+        if not saldo:
+            return None
+
         saldo.saldo_disponivel += Decimal(str(valor))
-        self.db.commit()
+
+        self.db.add(saldo)
         return saldo
 
-    def debitar(self, id_conta, valor):
+    def debit(self, id_conta, valor):
         # Passa o valor negativo para subtrair
         return self.update_saldo(id_conta, -Decimal(str(valor)))
 
-    def creditar(self, id_conta, valor):
+    def credit(self, id_conta, valor):
         # Passa o valor positivo para somar
         return self.update_saldo(id_conta, Decimal(str(valor)))
 
     def record_transaction(self, data: dict):
-        transacao = TransacaoModel(**data)
+        transacao = TransacaoModel(
+            id_conta_origem=data["id_conta_origem"],
+            id_conta_destino=data["id_conta_destino"],
+            tipo_transacao=data["tipo_transacao"],
+            valor=data["valor"]
+        )
         self.db.add(transacao)
-        self.db.commit()
-        self.db.refresh(transacao)
+        self.db.flush()
+
         return transacao
