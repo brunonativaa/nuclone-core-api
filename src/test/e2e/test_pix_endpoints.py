84938cce123f576b7ba2cdeea2bea26c3da9d1c4
Bruno <brunoferreirarodrigues26@outlook.com>
2026-08-18 11:11:15 -0300

    feat: indentificando os endpoints da API

---
+++ src/test/e2e/test_pix_endpoints.py
@@ -0,0 +1,17 @@
+import pytest
+from fastapi.testclient import TestClient
+from src.main import app
+
+
+cliente = TestClient(app)
+
+
+@pytest.mark.e2e
+def test_endpoint_realizar_pix_sem_autenticacao():
+    response = cliente.post("/pix/v1/transferir", json={
+        "id_conta_origem": 1,
+        "id_conta_destino": 2,
+        "valor": 100.00
+    })
+
+    assert response.status_code == 200
